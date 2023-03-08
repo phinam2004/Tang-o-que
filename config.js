51a2bd66cccf982e978241fa2b1ec9299040ee4f
phinam2004 <126351620+phinam2004@users.noreply.github.com>
2023-03-08 14:11:12 +0700

    tangoque

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'O Quế';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100071175596452';
+const eventName = 'Chúc Mừng 8-3';
+const titleCard = 'Tặng O Quế';
+const contentCard = 'Chúc O của con 8/3 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với O trong hôm nay và cả những ngày sau';
+
+
+const giftImage = 'o que.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
